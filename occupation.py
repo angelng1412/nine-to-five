259197f07db00c0411c3b2e003349fbf1b1bbbc1
angelng1412 <ang7@stuy.edu>
2017-09-17 20:03:58 -0400

    weighted random selection

--- occupation.py
+++ occupation.py
@@ -1,15 +1,27 @@
-import csv
+import random 
 
 f = open("occupations.csv", 'r')
 s = f.read()
-s = s.split("\r\n")
-del s[0]
-del s[-1]
-del s[-1]
+data = s.split("\n")
+data = data[1:-1]
 
 jobDict = {}
+for job in data:
+    if job.find('"') >= 0:
+        thing = job.split('"')
+        jobDict[thing[1]] = float(thing[2][1:])
+    else:
+        thing = job.split(',')
+        jobDict[thing[0]] = float(thing[1])
+#print jobDict
 
-for thing in s:
-    print thing 
-    
-#print s
+def randJob():
+    num = random.uniform(0,99.8)
+    tmp = 0 
+    for job, percent in jobDict.iteritems():
+        tmp += percent
+        if num < tmp:
+            return job
+
+for x in range(100):
+    print randJob()
